8ffa6b5ed9a888484efa95d0840dde45784e4fb3
codetector <codetector@codetector.org>
2025-02-27 23:02:28 -0800

    try 2

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,4 @@ pub use proc_macro::rename_future;
 pub use elain::Align;
 
 pub struct PhantomUnsend(core::marker::PhantomData<*const ()>);
-unsafe impl Sync for PhantomUnsend {}
+unsafe impl core::marker::Sync for PhantomUnsend {}
